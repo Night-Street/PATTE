5251c181d65c66f2e30c35c8cc95c6db8dba6b17
Night-Street <15850526434@163.com>
2022-03-21 19:46:03 +0800

    add images

--- programs/1022.cpp
+++ programs/1022.cpp
@@ -9,6 +9,10 @@
 
 int N, M, L;
 typedef long long ll;
+#define maxn 105
+bool werewolf[maxn];
+bool liar[maxn];
+int words[maxn];
 
 void init() {
 
@@ -16,10 +20,70 @@ void init() {
 
 void read() {
     std::cin >> N >> M >> L;
+    for (int i = 1; i <= N; ++i) {
+        scanf("%d", &words[i]);
+    }
 }
 
-void solve() {
+bool judge() {
+    if()
+    return true;
+}
 
+void output_werewolves() {
+    bool f = false;
+    for (int i = N; i >= 1; --i) {
+        if (werewolf[i]) {
+            printf("%s%d", f ? " " : "", i);
+            f = true;
+        }
+    }
+    putchar(10);
+}
+void output_liars() {
+    bool f = false;
+    for (int i = N; i >= 1; --i) {
+        if (liar[i]) {
+            printf("%s%d", f ? " " : "", i);
+            f = true;
+        }
+    }
+    putchar(10);
+}
+
+void dfs_M(int cur_cnt, int pos) {
+    werewolf[pos] = true;
+    cur_cnt++;
+    if (cur_cnt == M && judge()) {
+        output_werewolves();
+    } else {
+        for (int i = pos - 1; i >= 1; --i) {
+            dfs_M(cur_cnt, i);
+        }
+    }
+    cur_cnt--;
+    werewolf[pos] = false;
+}
+void dfs_L(int cur_cnt, int pos) {
+    liar[pos] = true;
+    cur_cnt++;
+    if (cur_cnt == L && judge()) {
+        output_liars();
+    } else {
+        for (int i = pos - 1; i >= 1; --i) {
+            dfs_L(cur_cnt, i);
+        }
+    }
+    cur_cnt--;
+    liar[pos] = false;
+}
+void solve() {
+    for (int i = N; i >= L - 1; --i) {
+        dfs_M(0, i);
+    }
+    for (int i = N; i >= L - 1; --i) {
+        dfs_L(0, i);
+    }
 }
 
 int main() {
